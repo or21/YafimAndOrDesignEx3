bcf9fc33a2b22b7b6ea1e8cfa9038443bbb9a20b
or21 <orbrand21@gmail.com>
2016-01-14 22:33:52 +0200

    new

---
+++ A16 Ex03 Or 302521034 Yafim 308973882/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace A16_Ex03_Or_302521034_Yafim_308973882
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
